a66d7f3fd715f97f963ad72b916501cf0dae0b6a
مصطفى خالد <atyaty003@gmal.com>
2024-08-21 15:40:43 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 const router = createBrowserRouter([
   {
     element: <AppLayout></AppLayout>,
+    errorElement: <Error></Error>,
     children: [
       {
         path: "/",
@@ -29,6 +31,7 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order></Order>,
+        loader: orderLoader,
       },
     ],
   },
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -3,7 +3,7 @@ import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
-
+ 
   return (
     <ul>
       {menu.map((e, k) => {
