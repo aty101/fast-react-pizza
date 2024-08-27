84c5c2b11c61101885e83e8cb112fc5cbcc4206a
aty101 <atyaty003@gmail.com>
2024-08-28 01:10:00 +0300

    styling

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -5,7 +5,7 @@ function Menu() {
   const menu = useLoaderData();
  
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((e, k) => {
         return <MenuItem pizza={e} key={k}></MenuItem>;
       })}
